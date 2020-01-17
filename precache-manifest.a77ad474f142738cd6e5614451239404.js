1e47e13a1033bc982375a50ef05a81e9784d8e58
Dima <ds081181@gmail.com>
2020-01-17 11:38:10 +0200

    Updates

---
+++ precache-manifest.a77ad474f142738cd6e5614451239404.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "982c4558048334b08f9fcb5fd60fbfb8",
+    "url": "/app-co/index.html"
+  },
+  {
+    "revision": "5454be6305895cedadeb",
+    "url": "/app-co/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dc705490b67a3dfb5da4",
+    "url": "/app-co/static/js/2.ade940fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/app-co/static/js/2.ade940fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5454be6305895cedadeb",
+    "url": "/app-co/static/js/main.4aefdfb5.chunk.js"
+  },
+  {
+    "revision": "55b5507c54e9534e907d",
+    "url": "/app-co/static/js/runtime-main.cd26cd7d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app-co/static/media/logo.5d5d9eef.svg"
+  }
+]);
